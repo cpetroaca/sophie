bf800aeb25156d53a17131a8f2a3dc949a6a5c28
Cristian Petroaca <cristian.petroaca@gmail.com>
2018-04-07 22:29:00 +0300

    Fix naming.

--- sophie/dep_utils.py
+++ sophie/dep_utils.py
@@ -1,22 +1,22 @@
 from . import constants
 
-def getSubjectsOfVerb(verb):
+def get_subjects_of_verb(verb):
     subjs = [token for token in verb.lefts
              if token.dep_ in constants.SUBJ_DEPS]
     # get additional conjunct subjects
-    subjs.extend(token for subj in subjs for token in getConjuncts(subj))
+    subjs.extend(token for subj in subjs for token in _get_conjuncts(subj))
     return subjs
     
-def getObjectsOfVerb(verb):
+def get_objects_of_verb(verb):
     objs = [token for token in verb.rights
             if token.dep_ in constants.OBJ_DEPS]
     # get open clausal complements (xcomp)
     objs.extend(token for token in verb.rights
                 if token.dep_ == 'xcomp')
     # get additional conjunct objects
-    objs.extend(token for obj in objs for token in getConjuncts(obj))
+    objs.extend(token for obj in objs for token in _get_conjuncts(obj))
     return objs
 
-def getConjuncts(token):
+def _get_conjuncts(token):
     return [right for right in token.rights
             if right.dep_ == 'conj']
--- sophie/parse.py
+++ sophie/parse.py
@@ -6,7 +6,7 @@ class Extractor:
         self.nlp = spacy.load(model)
         print("Loaded model '%s'" % model)
     
-    def getRelations(self, text):
+    def get_relations(self, text):
         doc = self.nlp(text)
         sents = doc.sents
         ents = doc.ents
@@ -15,22 +15,22 @@ class Extractor:
             for token in sent:
                 if token.dep_ == 'ROOT' and token.pos_ == 'VERB':
                     verb = token.lemma_
-                    subjs = dep_utils.getSubjectsOfVerb(token)
+                    subjs = dep_utils.get_subjects_of_verb(token)
                     if not subjs:
                         continue
-                    objs = dep_utils.getObjectsOfVerb(token)
+                    objs = dep_utils.get_objects_of_verb(token)
                     if not objs:
                         continue
                     
                     for subj in subjs:
                         for obj in objs:
-                            subjNerSpan = self.getNerSpan(ents, subj)
-                            objNerSpan = self.getNerSpan(ents, obj)
+                            subj_ner_span = self.get_ner_span(ents, subj)
+                            obj_ner_span = self.get_ner_span(ents, obj)
                             
-                            if (subjNerSpan is not None and objNerSpan is not None):
-                                yield (subjNerSpan, verb, objNerSpan)
+                            if (subj_ner_span is not None and obj_ner_span is not None):
+                                yield (subj_ner_span, verb, obj_ner_span)
     
-    def getNerSpan(self, ents, token):
+    def get_ner_span(self, ents, token):
         for ent in ents:
             if token.i >= ent.start and token.i <= ent.end:
                 return ent
