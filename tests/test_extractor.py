fd9cf1c33304937b60834ca3edbdf13a6b6fa472
Cristian Petroaca <cristian.petroaca@gmail.com>
2018-04-13 20:57:51 +0300

    Added more tests

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -3,18 +3,55 @@ import spacy
 from sophie import Extractor
 
 class TestRelationsExtraction(unittest.TestCase):
-    def test_simple_relation(self):
-        text = 'Barack Obama visited China'
-        nlp = spacy.load('en_core_web_sm')
-        extractor = Extractor()
+    def setUp(self):
+        self.spacyNlp = spacy.load('en_core_web_sm')
+        self.extractor = Extractor()
         
-        relations = extractor.get_relations(text)
-        doc = nlp(text)
+        super(TestRelationsExtraction, self).setUp()
+        
+    def test_simple_relation(self):
+        text = 'Barack Obama visited China.'
+        
+        relations = self.extractor.get_relations(text)
+        doc = self.spacyNlp(text)
         
         for relation in relations:
             self.assertEqual(relation[0], doc[0:2])
             self.assertEqual(relation[1], 'visit')
             self.assertEqual(relation[2], doc[3:4])
 
+    def test_negation(self):
+        text = 'Barack Obama did not visit China.'
+        
+        relations = self.extractor.get_relations(text)
+        relation_number = 0
+        
+        for relation in relations:
+            relation_number += 1
+            
+        self.assertEqual(relation_number, 0)
+    
+    def test_modal_verb(self):
+        text = 'Barack Obama should visit China.'
+        
+        relations = self.extractor.get_relations(text)
+        relation_number = 0
+        
+        for relation in relations:
+            relation_number += 1
+            
+        self.assertEqual(relation_number, 0)
+    
+    def test_no_ners(self):
+        text = 'I visited another country.'
+        
+        relations = self.extractor.get_relations(text)
+        relation_number = 0
+        
+        for relation in relations:
+            relation_number += 1
+            
+        self.assertEqual(relation_number, 0)
+    
 if __name__ == '__main__':
     unittest.main()
